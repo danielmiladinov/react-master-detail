e5df875ddf3858e7458fdd3b1b491fa9db546f92
Dustin Getz <dgetz@wingspan.com>
2014-01-29 18:05:48 -0500

    Added a second form and got it working

--- js/Page.js
+++ js/Page.js
@@ -1,9 +1,10 @@
 /** @jsx React.DOM */
 define([
-    'underscore', 'react', 'kendo', 'AutoForm', 'MasterDetail',
+    'underscore', 'react', 'kendo', 'util',
+    'AutoForm', 'MasterDetail',
     'text!textassets/types/Contact.json',
     'text!textassets/contacts.json'
-], function (_, React, kendo, AutoForm, MasterDetail,
+], function (_, React, kendo, util, AutoForm, MasterDetail,
              ContactModel, contacts) {
     'use strict';
 
@@ -19,6 +20,10 @@ define([
                 a: {
                     b: {
                         formValue: {} // value the editor is currently targetting
+                    },
+
+                    c: {
+                        formValue: {}
                     }
                 }
             };
@@ -30,25 +35,57 @@ define([
                     <MasterDetail
                         model={ContactModel} collection={this.state.collection}
                         formValue={this.state.a.b.formValue} onFormChange={this.onChangeABForm} />
-                    <button onClick={this.onFormSave}>Save</button>
+                    <button onClick={this.onFormSaveAB}>Save</button>
+                    <MasterDetail
+                        model={ContactModel} collection={this.state.collection}
+                        formValue={this.state.a.c.formValue} onFormChange={this.onChangeACForm} />
+                    <button onClick={this.onFormSaveAC}>Save</button>
                     <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
                 </div>
             );
         },
 
         onChangeABForm: function (value) {
-            this.setState({ a: { b: { formValue: value }}});
+            var nextState = util.deepClone(this.state);
+            nextState.a.b.formValue = value;
+            this.setState(nextState);
         },
 
-        onFormSave: function () {
-            var nextCollection = $.extend(true, [], this.state.collection);
-            var nextRecord = _.findWhere(nextCollection, { id: this.state.a.b.formValue.id });
-            // the record is a ref into the collection, mutate it
-            _.extend(nextRecord, this.state.a.b.formValue, { revision: this.state.a.b.formValue.revision + 1 });
-            this.setState({
-                collection: nextCollection,
-                a: { b: { formValue: nextRecord }}
-            });
+        onFormSaveAB: function () {
+            var record = _.findWhere(this.state.collection, { id: this.state.a.b.formValue.id });
+            var nextRecord = $.extend(true, {}, record, this.state.a.b.formValue, { revision: this.state.a.b.formValue.revision + 1 });
+
+            // subtract out the stale record (old revision)
+            // union in the new record into the nextCollection
+            var nextCollection = util.differenceDeep(this.state.collection, [record]);
+            nextCollection = util.unionDeep(nextCollection, [nextRecord]);
+
+            var nextState = util.deepClone(this.state);
+            nextState.collection = nextCollection;
+            nextState.a.b.formValue = nextRecord;
+            this.setState(nextState);
+        },
+
+        onChangeACForm: function (value) {
+            var nextState = util.deepClone(this.state);
+            nextState.a.c.formValue = value;
+            this.setState(nextState);
+        },
+
+        onFormSaveAC: function () {
+            var record = _.findWhere(this.state.collection, { id: this.state.a.c.formValue.id });
+            var nextRecord = $.extend(true, {}, record, this.state.a.c.formValue, { revision: this.state.a.c.formValue.revision + 1 });
+
+            // subtract out the stale record (old revision)
+            // union in the new record into the nextCollection
+            var nextCollection = util.differenceDeep(this.state.collection, [record]);
+            nextCollection = util.unionDeep(nextCollection, [nextRecord]);
+
+
+            var nextState = util.deepClone(this.state);
+            nextState.collection = nextCollection;
+            nextState.a.c.formValue = nextRecord;
+            this.setState(nextState);
         }
     });
 
